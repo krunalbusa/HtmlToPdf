88d97ba29db4cb092f8aa04b6c4896b73b8ecb67
Busa Krunal <krunalbusa123@gmail.com>
2024-03-01 11:44:04 +0530

    ok

--- obj/Release/net6.0/ConvertToPDF.AssemblyInfo.cs
+++ obj/Release/net6.0/ConvertToPDF.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConvertToPDF")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7846ce058c4763102d0bbc272a940e3ee2e63afc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConvertToPDF")]
